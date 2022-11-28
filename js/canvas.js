555c2a1b3d3f607108eeaa8e365c2ccc0f424db7
Olesya-Trubnikova-Frontend <113354008+Olesya-Trubnikova-Frontend@users.noreply.github.com>
2022-11-28 16:11:38 +0300

    Последняя версия

--- js/canvas.js
+++ js/canvas.js
@@ -1,57 +1,59 @@
-const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"];
-const BUBBLE_DENSITY = 100;
+const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"]; // цвета пузырьков
+const BUBBLE_DENSITY = 100; // количество пузырьков
 
-  function generateDecimalBetween(left, right) {
+  function generateDecimalBetween(left, right) { // начальная позиция пузырька, его размер, скорость движения вверх, прозрачность цвета
       return (Math.random() * (left - right) + right).toFixed(2);
     }
 
-  class Bubble {
+  class Bubble { // размер, цвет, положение пузырьков
   constructor(canvas) {
     this.canvas = canvas;
 
-    this.getCanvasSize();
+    this.getCanvasSize(); // вызывает метод
 
-    this.init();
+    this.init(); // вызывает метод
   }
 
   getCanvasSize() {
-    this.canvasWidth = this.canvas.clientWidth;
-    this.canvasHeight = this.canvas.clientHeight;
+    // вытаскивает из холста его размеры и сохраняет в переменные
+    this.canvasWidth = this.canvas.clientWidth; // расчет положения точки на холсте
+    this.canvasHeight = this.canvas.clientHeight; // расчет положения точки на холсте
   }
 
   init() {
-    this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
+    this.color = COLORS[Math.floor(Math.random() * COLORS.length)]; // случайный цвет пузырьков
     this.alpha = generateDecimalBetween(5, 10) / 10;
-    this.size = generateDecimalBetween(1, 3);
-    this.translateX = generateDecimalBetween(0, this.canvasWidth);
-    this.translateY = generateDecimalBetween(0, this.canvasHeight);
-    this.velocity = generateDecimalBetween(20, 40);
-    this.movementX = generateDecimalBetween(-2, 2) / this.velocity;
-    this.movementY = generateDecimalBetween(1, 20) / this.velocity;
+    this.size = generateDecimalBetween(1, 3); // размер пузырьков
+    this.translateX = generateDecimalBetween(0, this.canvasWidth); // начальное положение на холсте
+    this.translateY = generateDecimalBetween(0, this.canvasHeight); // начальное положение на холсте
+    this.velocity = generateDecimalBetween(20, 40); // значение скорости
+    this.movementX = generateDecimalBetween(-2, 2) / this.velocity; // движение пузырьков
+    this.movementY = generateDecimalBetween(1, 20) / this.velocity; // движение пузырьков
   }
 
   move() {
-   this.translateX = this.translateX - this.movementX;
-   this.translateY = this.translateY - this.movementY;
+   this.translateX = this.translateX - this.movementX; // обновляются координаты
+   this.translateY = this.translateY - this.movementY; // обновляются координаты
 
    if (this.translateY < 0 || this.translateX < 0 || this.translateX > this.canvasWidth) {
-     this.init();
+     // цикл не дает выйти за границы пузырькам и возвращает их обратно
+     this.init(); // вызывает метод
      this.translateY = this.canvasHeight;
-    }
+   }
   }
 }
 
-class CanvasBackground {
+class CanvasBackground { // рисует пузырьки на холсте и анимирует их
   constructor(id) {
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
     this.dpr = window.devicePixelRatio;
   }
-  
-  start() {
-    this.canvasSize();
-    this.generateBubbles();
-    this.animate();
+
+  start() { // запуск анимации
+    this.canvasSize(); // подстраивает размер
+    this.generateBubbles(); // создает пузырьки
+    this.animate(); // анимирует
   }
 
   canvasSize() {
@@ -61,20 +63,20 @@ class CanvasBackground {
     this.ctx.scale(this.dpr, this.dpr);
   }
 
-  animate() {
-    this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+  animate() { 
+    this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight); // очищаем холст
 
     this.bubblesList.forEach((bubble) => {
-       bubble.move();
-       this.ctx.translate(bubble.translateX, bubble.translateY);
-       this.ctx.beginPath();
-       this.ctx.arc(0, 0, bubble.size, 0, 2 * Math.PI);
-       this.ctx.fillStyle = "rgba(" + bubble.color + "," + bubble.alpha + ")";
-       this.ctx.fill();
-       this.ctx.setTransform(this.dpr, 0, 0, this.dpr, 0, 0);
-   });
-         requestAnimationFrame(this.animate.bind(this));
-}
+      bubble.move();
+      this.ctx.translate(bubble.translateX, bubble.translateY);
+      this.ctx.beginPath(); // начало отрисовки
+      this.ctx.arc(0, 0, bubble.size, 0, 2 * Math.PI); // рисуем круг
+      this.ctx.fillStyle = "rgba(" + bubble.color + "," + bubble.alpha + ")"; // закрашиваем круг
+      this.ctx.fill(); // закрашиваем круг
+      this.ctx.setTransform(this.dpr, 0, 0, this.dpr, 0, 0); // горизонтальное и вертикальное масштабирование
+    });
+    requestAnimationFrame(this.animate.bind(this)); // запуск анимации
+  }
 
   generateBubbles() {
     this.bubblesList = [];
@@ -83,5 +85,5 @@ class CanvasBackground {
     }
   }
 }
-const canvas = new CanvasBackground("orb-canvas");
-canvas.start();
+const canvas = new CanvasBackground("orb-canvas"); // создает class
+canvas.start(); // вызывает метод, запуск
